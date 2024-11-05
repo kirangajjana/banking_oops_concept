d01942d1b51bcb4c053c353e871af2b566cbd87b
kirangajjana <kiran.gajjana@gmail.com>
2024-11-05 21:18:45 +0530

    updated output

--- app.py
+++ app.py
@@ -16,33 +16,33 @@ class banking:
         self.balance=self.balance+Loan #user can opt for loan upto 5lakhs more than that need further approval
 user_name=input("please enter your name")   
 c=banking(user_name)
-print("D-Deposit\nW-Withdrawl\nL-loan\nE-exit") #user need to select from the below options to move further
-output=input("please enter the option you want to proceed") 
 while True:
+    print("D-Deposit\nW-Withdrawl\nL-loan\nE-exit") #user need to select from the below options to move further
+    output=input("please enter the option you want to proceed") 
     if output.lower()=="d": #checking the condition wether the person opted for deposite
-        print(f"hello mr {c.name}  user welcome to the {banking.bank} of branch {banking.branch}")
-        amount=int(input("enter the amount you want to deposit into your account"))
-        if amount>1:
-            d=c.deposit(amount)
-            print(f"the updated balance in your account is {d}")
-        else:
-            print("please enter the valid input")    
+            print(f"hello mr {c.name}  user welcome to the {banking.bank} of branch {banking.branch}")
+            amount=int(input("enter the amount you want to deposit into your account"))
+            if amount>1:
+                d=c.deposit(amount)
+                print(f"the updated balance in your account is {d}")
+            else:
+                print("please enter the valid input")    
     elif output.lower()=="w":  #checking the condition wether the person opted for withdraw
-        print(f"hello  mr {c.name} user welcome to the {banking.bank} of branch {banking.branch}")
-        amount=input("enter the amount you want to withdraw from  your account")
-        if amount>c.balance:
-            d=c.withdraw(amount)
-            print(f"the total amount you have withdrawn {d}")
-        else:
-            print("sorry you dont have the balance to go with the above transaction")
-            sys.exit()
+            print(f"hello  mr {c.name} user welcome to the {banking.bank} of branch {banking.branch}")
+            amount=input("enter the amount you want to withdraw from  your account")
+            if amount>c.balance:
+                d=c.withdraw(amount)
+                print(f"the total amount you have withdrawn {d}")
+            else:
+                print("sorry you dont have the balance to go with the above transaction")
+                sys.exit()
 
     elif output.lower()=="l":
-         print(f"hello mr {c.name} user welcome to the {banking.bank} of branch {banking.branch}")
-         amount=input("enter the amount you want to take loan from the bank")
-         sys.exit()
+            print(f"hello mr {c.name} user welcome to the {banking.bank} of branch {banking.branch}")
+            amount=input("enter the amount you want to take loan from the bank")
+            sys.exit()
 
 
-         
+            
 
 
