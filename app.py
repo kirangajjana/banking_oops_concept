f08113185f13a43d0826ba53a8cdc625cf6bc72f
kirangajjana <kiran.gajjana@gmail.com>
2024-11-05 20:51:54 +0530

    update1 app.py

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ class banking:
         self.balance=balance
     def deposit(self,amt):  #instance method
         self.balance=self.balance+amt  #user csn add money using this method
-
-
+        return self.balance
+    def withdraw(self,amt): #instance method
+        self.balance=self.balance-amt
+        return self.balance   
+    def loanservice(self,Loan): #instance method
+        self.balance=self.balance+Loan #user can opt for loan upto 5lakhs more than that need further approval
+user_name=input("please enter your name")   
+c=banking(user_name)
+print("D-Deposit\nW-Withdrawl\nL-loan\nE-exit")
+output=input("please enter the option you want to proceed")
